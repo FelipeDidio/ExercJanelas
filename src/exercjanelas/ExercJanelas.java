74d8e3e919faaa523f6b6e7dbf415c2c77718588
Felipe Verdade <felipeverdade@gmail.com>
2023-11-29 19:41:47 -0300

    subindo projeto para o github

---
+++ src/exercjanelas/ExercJanelas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercjanelas;
+
+import javax.swing.JFrame;
+import views.Janela1;
+
+/**
+ *
+ * @author 180701103
+ */
+public class ExercJanelas {
+
+    
+    public static void main(String[] args) {
+      
+      Janela1 j = new Janela1();
+      j.setVisible(true);
+      j.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+      //j.
+     // j.setSize(350, 200);
+    }
+    
+}
---
+++ src/models/Dados.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author 180701103
+ */
+public class Dados {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
